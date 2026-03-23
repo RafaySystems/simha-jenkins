89c256c94427540d56f7342ea30085f67337ae1e
Vijayrafay <94045257+Vijayrafay@users.noreply.github.com>
2026-03-23 20:32:56 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -5,6 +5,7 @@ aws ssm send-command \
   --instance-ids i-0e38286eeff640732 \
   --document-name "AWS-RunShellScript" \
   --parameters commands=["whoami","pwd"] \
-  --region us-west-1
+  --region us-west-1 \
+  --profile dev-noc
 date
 testbed-create-onprem
