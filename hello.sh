515fab53817b064ec061c4726286de7e412a2795
Vijayrafay <94045257+Vijayrafay@users.noreply.github.com>
2026-03-23 20:11:06 +0530

    Update hello.sh

--- hello.sh
+++ hello.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
 echo "Hello from Git Repo 🚀"
+aws ssm send-command \
+  --instance-ids i-0e38286eeff640732 \
+  --document-name "AWS-RunShellScript" \
+  --parameters commands=["whoami","pwd"] \
+  --region us-west-1 \
+  --profile dev-noc
 date
